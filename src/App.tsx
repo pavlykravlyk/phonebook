eb45ff720c5871fda69cf961b215f5ad507937bf
pavlykravlyk <kravchuk.pavlo@ukr.net>
2025-02-05 22:39:15 +0100

    fix bag

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { getIsRefreshing } from 'redux/auth';
 import { useAppSelector, useAuth } from 'redux/hooks';
 import { ToastContainer } from 'react-toastify';
